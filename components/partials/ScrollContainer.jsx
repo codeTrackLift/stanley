335b204c3537a129808776908a8c8c4f41900df8
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-07-04 11:59:13 -0400

    scrollContainer working

--- components/partials/ScrollContainer.jsx
+++ components/partials/ScrollContainer.jsx
@@ -5,14 +5,22 @@ import hvacTech from '../../images/hvacTech.webp'
 import whiteVans from '../../images/whiteVans.webp'
 import styles from '../../styles/scroll.module.scss'
 
+import InitScroll from '../../utils/initScroll'
+
 export default function ScrollContainer() {
+
+  InitScroll();
 
   return (
     <div id='scrollContainer' className={styles.scrollContainer}>
-      <Image className={styles.image} src={whiteVans} alt='HVAC technicians coming out of white vans' />
-      <Image className={styles.image} src={CarrierUnit} alt='Carrier HVAC unit' />
-      <Image className={styles.image} src={hvacTech} alt='HVAC technician working' />
-      <Image className={styles.image} src={hvacMeter} alt='HVAC technician using a meter' />
+      <Image className={`${styles.image} img-fluid`} width={500} height={300} src={whiteVans} alt='HVAC technicians coming out of white vans' />
+      <Image className={`${styles.image} img-fluid`} width={500} height={300} src={CarrierUnit} alt='Carrier HVAC unit' />
+      <Image className={`${styles.image} img-fluid`} width={500} height={300} src={hvacTech} alt='HVAC technician working' />
+      <Image className={`${styles.image} img-fluid`} width={500} height={300} src={hvacMeter} alt='HVAC technician using a meter' />
+      <Image className={`${styles.image} img-fluid`} width={500} height={300} src={whiteVans} alt='HVAC technicians coming out of white vans' />
+      <Image className={`${styles.image} img-fluid`} width={500} height={300} src={CarrierUnit} alt='Carrier HVAC unit' />
+      <Image className={`${styles.image} img-fluid`} width={500} height={300} src={hvacTech} alt='HVAC technician working' />
+      <Image className={`${styles.image} img-fluid`} width={500} height={300} src={hvacMeter} alt='HVAC technician using a meter' />
     </div>
   )
 }
--- utils/initScroll.js
+++ utils/initScroll.js
@@ -1,26 +1,32 @@
-export default function initScroll() {
-  const scrollContainer = document.getElementById('scrollContainer');
-  const scrollContainerWidth = scrollContainer.scrollWidth;
-  
-  const scroll = () => {
-      const first = document.querySelector('#scrollContainer p');
-      
-      if (!isElementInViewport(first)) {
-          scrollContainer.appendChild(first);
-          scrollContainer.scrollTo(scrollContainer.scrollLeft - first.offsetWidth, 0);
-      }
-  
-      if (scrollContainer.scrollLeft !== scrollContainerWidth) {
-          scrollContainer.scrollTo(scrollContainer.scrollLeft + 1, 0);
-      } 
+import React, {useEffect} from "react";
 
-  };
-  
-  clearInterval(scroll);
-  setInterval(scroll, 15);
-  
-  function isElementInViewport(el) {
-      var rect = el.getBoundingClientRect();
-      return rect.right > 0;
-  }
+export default function InitScroll() {
+
+  useEffect(() => {
+
+    const scrollContainer = document.getElementById('scrollContainer');
+    const scrollContainerWidth = scrollContainer.scrollWidth;
+    
+    const scroll = () => {
+        const first = document.querySelector('#scrollContainer *');
+        
+        if (!isElementInViewport(first)) {
+            scrollContainer.appendChild(first);
+            scrollContainer.scrollTo(scrollContainer.scrollLeft - first.offsetWidth, 0);
+        }
+    
+        if (scrollContainer.scrollLeft !== scrollContainerWidth) {
+            scrollContainer.scrollTo(scrollContainer.scrollLeft + 1, 0);
+        } 
+
+    };
+    
+    clearInterval(scroll);
+    setInterval(scroll, 15);
+    
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return rect.right > 0;
+    }
+  }, [])
 }
